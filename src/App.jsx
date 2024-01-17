3f76a466839cf1dc098f611ade939f9326874db7
RokasV <vezikauskas.r@gmail.com>
2024-01-17 08:57:57 +0200

    Todo boiler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import TodoSection from './components/todos/TodoSection';
 import './styles/App.css';
 
 export default function App() {
   return (
     <div className='App container'>
-      <h1>Hello, world!</h1>
+      <h1>Hello, Todo</h1>
       <hr />
+      <TodoSection />
     </div>
   );
 }
